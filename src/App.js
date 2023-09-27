7b780605747cd0f46b3bc53f1c8bbb17bc16e5a9
CJHansen10 <chanse25@lion.lmu.edu>
2023-09-27 12:40:34 -0700

    changed code in js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./styles.css";
+import Title from "./Title.js";
+import Entry from "./Entry.js";
+import Info from "./Info.js";
 
-function App() {
+export default function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const pokemon = encodeURIComponent(name.toLowerCase());
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    console.log(url);
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => setData(r))
+      .catch((e) => setData(e));
+  }, [name]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Poké Finder" />
+      <Entry action={setName} />
+      <Info name={name} data={data} />
     </div>
   );
 }
-
-export default App;
