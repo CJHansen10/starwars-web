e1dc7ae48949b89ea01ce557daea1387ac56ecc8
CJHansen10 <chanse25@lion.lmu.edu>
2023-10-05 17:59:38 -0700

    displayed error message when no network and moved around elements

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Entry from './Entry.js'
 import Info from './Info.js'
 import { getStarWars } from './api'
 import dancing_3po2 from './images/dancing_3po2.gif'
+import dancing_3po3 from './images/dancing_3po3.gif'
 
 export default function App() {
   const [name, setName] = useState('')
   const [data, setData] = useState('')
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
 
   useEffect(() => {
     // console.log(name)
@@ -17,11 +19,17 @@ export default function App() {
     if (name) {
       const fetchData = async () => {
         setLoading(true)
-        const data = await getStarWars(name)
-        // console.log('APP > data: ', data)
+        try {
+          const data = await getStarWars(name)
 
-        setData(data)
-        setLoading(false)
+          // console.log('APP > data: ', data)
+
+          setData(data)
+          setLoading(false)
+        } catch (error) {
+          console.error('ERRRORRRRR')
+          setError('NO NETWORK CONNECTION')
+        }
       }
 
       fetchData()
@@ -33,13 +41,16 @@ export default function App() {
 
   return (
     <div className="App">
-      <Title text="Star Wars Film Characters 1-6!" />
-      <Entry action={setName} />
       {/* {loading && <loader />} */}
 
-      {loading ? 'LOADING' : <Info name={name} data={data} />}
-      <img src={dancing_3po2} class="image1"/>
-      <img src={dancing_3po3} class="image2"/>
+      <img src={dancing_3po2} class="image1" />
+      <div className="MiddlePart">
+        <Title text="Star Wars Film Characters 1-6!" className="title" />
+        <Entry action={setName} />
+        {loading ? 'LOADING' : <Info name={name} data={data} />}
+      </div>
+      <img src={dancing_3po3} class="image2" />
+      {error && <div>{error}</div>}
     </div>
   )
 }
